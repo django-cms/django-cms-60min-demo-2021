625366e8f7283e240ec3fc8d74e6b927299c2e63
Victor Yunenko <victor@what.digital>
2020-04-11 12:17:47 +0300

    drop redundant comments

--- frontend/vendor/index.js
+++ frontend/vendor/index.js
@@ -1,7 +1,6 @@
 import * as Sentry from '@sentry/browser';
 
 
-// this will let us have a global jQuery object
 window.$ = window.jQuery = require("jquery");
 
 
@@ -20,28 +19,3 @@ Sentry.init({
     dsn: '',
     environment: DJANGO.env,
 });
-
-
-// DEMO tools that frontend engineers like
-// if you want to install one of them cd to the frontend directory and: npm i <name> --save
-
-// window.enquire = require('enquire.js');
-// window.FastClick = require('fastclick');
-// require('jquery-debouncedresize');
-// require('imagesloaded');
-// require('jquery-match-height');
-// window.Cookies = require('js-cookie');
-// window.svg4everybody = require('svg4everybody');
-// window.objectFitImages = require('object-fit-images');
-// window.autosize = require('autosize');
-
-// require('slick-carousel/slick/slick.scss');
-// require('slick-carousel/slick/slick-theme.scss');
-// require('slick-carousel');
-
-
-// Modernizr global :)
-// window.Modernizr = require('modernizr');
-
-// window.moment = require('moment');
-// require('moment-timezone');
