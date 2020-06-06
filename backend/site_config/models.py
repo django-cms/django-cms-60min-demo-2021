9eba5a0061af797bf04cc55b9920e6a13e9c4383
Victor Yunenko <victor@what.digital>
2020-06-06 22:36:03 +0300

    site config: add subtitle

--- backend/site_config/models.py
+++ backend/site_config/models.py
@@ -7,6 +7,11 @@ from solo.models import SingletonModel
 class SiteConfig(SingletonModel, TranslatableModel):
     translations = TranslatedFields(
         site_name=models.CharField(max_length=1024),
+        site_subname=models.CharField(
+            max_length=1024,
+            help_text="Shown in the navbar below the name.",
+            blank=True,
+        ),
     )
 
     def __str__(self) -> str:
