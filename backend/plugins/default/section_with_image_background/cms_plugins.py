2431aaeca2936a1577c0b16662b08e1bdb08cd18
Victor Yunenko <victor@what.digital>
2020-06-06 22:20:40 +0300

    allow to configure section background

--- backend/plugins/default/section_with_image_background/cms_plugins.py
+++ backend/plugins/default/section_with_image_background/cms_plugins.py
@@ -1,5 +1,7 @@
+from cms.models import CMSPlugin
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
+from cms.utils.plugins import get_plugin_model
 from django.utils.translation import ugettext_lazy as _
 
 from backend.plugins.default.module_name import MODULE_NAME
@@ -13,3 +15,21 @@ class SectionWithImageBackgroundPlugin(CMSPluginBase):
     name = _("Section with image background")
     render_template = 'section_with_image_background/section_with_image_background.html'
     allow_children = True
+
+    fieldsets = (
+        (None, {
+            'description': "You can add plugins inside this plugin, eg a text or heading plugin.",
+            'fields': [
+                'name',
+                'background_image',
+                'height',
+                'rich_text_editor_background',
+            ]
+        }),
+    )
+
+    @classmethod
+    def get_child_ckeditor_body_css_class(cls, plugin: CMSPlugin) -> str:
+        plugin_model = get_plugin_model(plugin.plugin_type)
+        plugin_instance: SectionWithImageBackgroundPluginModel = plugin_model.objects.get(pk=plugin.pk)
+        return f'background-{plugin_instance.rich_text_editor_background.value}'
--- backend/plugins/default/section_with_image_background/models.py
+++ backend/plugins/default/section_with_image_background/models.py
@@ -1,6 +1,14 @@
 from cms.models.pluginmodel import CMSPlugin
 from django.db import models
+from enumfields import Enum
+from enumfields import EnumField
 from filer.fields.image import FilerImageField
+
+
+class RichTextEditorBackground(Enum):
+    WHITE = 'white'
+    BLACK = 'black'
+    GRAY = 'gray'
 
 
 class SectionWithImageBackgroundPluginModel(CMSPlugin):
@@ -10,6 +18,14 @@ class SectionWithImageBackgroundPluginModel(CMSPlugin):
         help_text="Minimal width is 1920px, default minimal height is 350px, but can be configured below."
     )
     height = models.IntegerField(default=350, help_text="In pixels")
+    rich_text_editor_background = EnumField(
+        RichTextEditorBackground,
+        default=RichTextEditorBackground.WHITE,
+        help_text=(
+            "You can set the background of a rich text editor used inside this plugin, for readability purposes. "
+            "For example if your background image is black it's better to set background in the editor to be black as well."
+        ),
+    )
 
     def get_size(self) -> str:
         return f'2560x{self.height}'
