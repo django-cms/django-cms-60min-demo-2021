72a20338c4dee648bf0c76d0e4b50861684ca200
Victor Yunenko <victor@what.digital>
2020-07-19 14:06:48 +0300

    menu: keep open when active (draft)

--- frontend/global/ts/main-menu.ts
+++ frontend/global/ts/main-menu.ts
@@ -5,8 +5,31 @@ require('metismenujs/dist/metismenujs.min.css');
 export function initMainMenu() {
     const isMenuPresent = document.getElementById('menu') !== null;
     if (isMenuPresent) {
-        new MetisMenu('#menu', {
-            toggle: false,
-        });
+        new MetisMenu('#menu', {toggle: false});
+        activeCurrentMenuNodes();
+    }
+}
+
+
+function activeCurrentMenuNodes() {
+    activateCurrentMenuNode('.selected');
+    activateCurrentMenuNode('.ancestor');
+}
+
+
+function activateCurrentMenuNode(selector: string) {
+    const activeElems = document.querySelectorAll(selector) as any as Array<HTMLElement>;
+    for (let activeElem of activeElems) {
+        const selectElemParent = activeElem.parentElement as HTMLElement;
+        if (selectElemParent.tagName === 'UL') {
+            // show list
+            selectElemParent.classList.add('mm-show');
+            
+            // activate expand mode
+            const selectedParentParent = selectElemParent.parentElement as HTMLElement;
+            const selectedContainerAElem = selectedParentParent.querySelector('a') as HTMLElement;
+            selectedContainerAElem.setAttribute('aria-expanded', String(true));
+            selectedContainerAElem.classList.remove('mm-collapsed');
+        }
     }
 }
