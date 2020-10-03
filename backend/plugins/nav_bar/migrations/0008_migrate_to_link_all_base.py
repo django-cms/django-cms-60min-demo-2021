a19e0880996a2c5a5c66dc00a2b34ea650d22626
Victor Yunenko <victor@what.digital>
2020-10-03 17:10:18 +0300

    nav_bar: make the existing migrations runnable without an old db

--- backend/plugins/nav_bar/migrations/0008_migrate_to_link_all_base.py
+++ backend/plugins/nav_bar/migrations/0008_migrate_to_link_all_base.py
@@ -14,10 +14,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='menuitemmodel',
-            name='link',
-        ),
         migrations.AddField(
             model_name='menuitemmodel',
             name='link_button_color',
